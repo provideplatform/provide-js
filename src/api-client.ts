3ceabea45e341e0c7d7c6bfa07a667ba72979ff5
Philip Keiter <FiretronP75@users.noreply.github.com>
2018-09-22 17:25:07 -0400

    Changed to vanilla js instead of request.

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,5 +1,3 @@
-import { RequestAPI } from "request";
-
 export class ApiClient {
 
   private static readonly DEFAULT_SCHEME = 'https';
@@ -7,8 +5,7 @@ export class ApiClient {
   private static readonly DEFAULT_PATH = 'api';
 
   public baseUrl: string;
-  public headers: object;
-  public requestAPI: RequestAPI<any, any, any> = require('request-promise-native');
+  public headersInit: HeadersInit;
 
   constructor(token: string, scheme?: string, host?: string, path?: string) {
     if (!scheme) scheme = ApiClient.DEFAULT_SCHEME;
@@ -16,49 +13,64 @@ export class ApiClient {
     if (!path) path = ApiClient.DEFAULT_PATH;
 
     this.baseUrl = `${scheme}://${host}/${path}/`;
-    this.headers = {
-      'user-agent': 'provide-js client',
-      'authorization': `bearer ${token}`,
+    this.headersInit = {
+      "Content-Type": "application/json; charset=utf-8",
+      'User-Agent': 'provide-js client',
+      'Authorization': `bearer ${token}`,
     };
+  }
+
+  private static toQuery(params: object): string {
+    const paramList = [];
+
+    for (const p in params)
+      if (params.hasOwnProperty(p))
+        paramList.push(encodeURIComponent(p) + "=" + encodeURIComponent(params[p]));
+
+    if (paramList.length > 0)
+      return '?' + paramList.join("&");
+    else
+      return '';
   }
 
   public get(uri: string, params: object): Promise<any> {
-    const options = {
-      headers: this.headers,
-      qs: params,
-      json: true,
+    const requestInit: RequestInit = {
+      method: 'GET',
+      headers: this.headersInit,
     };
 
-    return this.requestAPI.get(uri, options);
+    const uriWithQuery = uri + ApiClient.toQuery(params);
+
+    return fetch(this.baseUrl + uriWithQuery, requestInit).then(response => response.json());
   }
 
   public post(uri: string, params: object): Promise<any> {
-    const options = {
-      headers: this.headers,
-      body: params,
-      json: true,
+    const requestInit: RequestInit = {
+      method: 'POST',
+      headers: this.headersInit,
+      body: JSON.stringify(params),
     };
 
-    return this.requestAPI.post(uri, options);
+    return fetch(this.baseUrl + uri, requestInit).then(response => response.json());
   }
 
   public put(uri: string, params: object): Promise<any> {
-    const options = {
-      headers: this.headers,
-      body: params,
-      json: true,
+    const requestInit: RequestInit = {
+      method: 'PUT',
+      headers: this.headersInit,
+      body: JSON.stringify(params),
     };
 
-    return this.requestAPI.put(uri, options);
+    return fetch(this.baseUrl + uri, requestInit).then(response => response.json());
   }
 
   public delete(uri: string): Promise<any> {
-    const options = {
-      headers: this.headers,
-      json: true,
+    const requestInit: RequestInit = {
+      method: 'POST',
+      headers: this.headersInit,
     };
 
-    return this.requestAPI.delete(uri, options);
+    return fetch(this.baseUrl + uri, requestInit).then(response => response.json());
   }
 
 }
