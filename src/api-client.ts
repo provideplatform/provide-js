622160b75d75a4d972b3a54e4de86af9977e7159
Philip Keiter <FiretronP75@users.noreply.github.com>
2018-09-23 11:11:30 -0400

    Changed default path to api/v1 and changed mode to cors and removed json parse of response.

--- src/api-client.ts
+++ src/api-client.ts
@@ -2,7 +2,7 @@ export class ApiClient {
 
   private static readonly DEFAULT_SCHEME = 'https';
   private static readonly DEFAULT_HOST = 'provide.services';
-  private static readonly DEFAULT_PATH = 'api';
+  private static readonly DEFAULT_PATH = 'api/v1';
 
   public baseUrl: string;
   public headers: Headers;
@@ -15,7 +15,7 @@ export class ApiClient {
     this.baseUrl = `${scheme}://${host}/${path}/`;
 
     this.headers = new Headers();
-    this.headers.append('Content-Type', 'application/json; charset=utf-8');
+    this.headers.append('Content-Type', 'application/json');
     this.headers.append('User-Agent', 'provide-js client');
     this.headers.append('Authorization', `bearer ${token}`);
   }
@@ -37,48 +37,52 @@ export class ApiClient {
     const requestInit: RequestInit = {
       method: 'GET',
       headers: this.headers,
+      mode: "cors",
     };
 
     const uriWithQuery = uri + ApiClient.toQuery(params);
 
     const request = new Request(this.baseUrl + uriWithQuery);
 
-    return fetch(request, requestInit).then(response => response.json());
+    return fetch(request, requestInit);
   }
 
   public post(uri: string, params: object): Promise<any> {
     const requestInit: RequestInit = {
       method: 'POST',
       headers: this.headers,
+      mode: "cors",
       body: JSON.stringify(params),
     };
 
     const request = new Request(this.baseUrl + uri);
 
-    return fetch(request, requestInit).then(response => response.json());
+    return fetch(request, requestInit);
   }
 
   public put(uri: string, params: object): Promise<any> {
     const requestInit: RequestInit = {
       method: 'PUT',
       headers: this.headers,
+      mode: "cors",
       body: JSON.stringify(params),
     };
 
     const request = new Request(this.baseUrl + uri);
 
-    return fetch(request, requestInit).then(response => response.json());
+    return fetch(request, requestInit);
   }
 
   public delete(uri: string): Promise<any> {
     const requestInit: RequestInit = {
       method: 'DELETE',
       headers: this.headers,
+      mode: "cors",
     };
 
     const request = new Request(this.baseUrl + uri);
 
-    return fetch(request, requestInit).then(response => response.json());
+    return fetch(request, requestInit);
   }
 
 }
