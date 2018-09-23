9e3ca181cdbc3cebb7de9a49588a735513eb42e3
Philip Keiter <FiretronP75@users.noreply.github.com>
2018-09-23 13:05:46 -0400

    Fixed headers.

--- src/api-client.ts
+++ src/api-client.ts
@@ -5,7 +5,8 @@ export class ApiClient {
   private static readonly DEFAULT_PATH = 'api/v1';
 
   public baseUrl: string;
-  public headers: Headers;
+
+  private token: string;
 
   constructor(token: string, scheme?: string, host?: string, path?: string) {
     if (!scheme) scheme = ApiClient.DEFAULT_SCHEME;
@@ -14,10 +15,7 @@ export class ApiClient {
 
     this.baseUrl = `${scheme}://${host}/${path}/`;
 
-    this.headers = new Headers();
-    this.headers.append('Content-Type', 'application/json');
-    this.headers.append('User-Agent', 'provide-js client');
-    this.headers.append('Authorization', `bearer ${token}`);
+    this.token = token;
   }
 
   private static toQuery(params: object): string {
@@ -34,55 +32,75 @@ export class ApiClient {
   }
 
   public get(uri: string, params: object): Promise<any> {
-    const requestInit: RequestInit = {
-      method: 'GET',
-      headers: this.headers,
-      mode: "cors",
-    };
-
     const uriWithQuery = uri + ApiClient.toQuery(params);
 
-    const request = new Request(this.baseUrl + uriWithQuery);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('User-Agent', 'provide-js client');
+    headers.append('Authorization', `bearer ${this.token}`);
 
-    return fetch(request, requestInit);
+    const requestInit: RequestInit = {
+      method: 'GET',
+      headers: headers,
+      mode: 'cors'
+    };
+
+    const request = new Request(uriWithQuery, requestInit);
+
+    return fetch(request);
   }
 
   public post(uri: string, params: object): Promise<any> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('User-Agent', 'provide-js client');
+    headers.append('Authorization', `bearer ${this.token}`);
+
     const requestInit: RequestInit = {
       method: 'POST',
-      headers: this.headers,
-      mode: "cors",
+      headers: headers,
+      mode: 'cors',
       body: JSON.stringify(params),
     };
 
-    const request = new Request(this.baseUrl + uri);
+    const request = new Request(this.baseUrl + uri, requestInit);
 
-    return fetch(request, requestInit);
+    return fetch(request);
   }
 
   public put(uri: string, params: object): Promise<any> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('User-Agent', 'provide-js client');
+    headers.append('Authorization', `bearer ${this.token}`);
+
     const requestInit: RequestInit = {
       method: 'PUT',
-      headers: this.headers,
-      mode: "cors",
+      headers: headers,
+      mode: 'cors',
       body: JSON.stringify(params),
     };
 
-    const request = new Request(this.baseUrl + uri);
+    const request = new Request(this.baseUrl + uri, requestInit);
 
-    return fetch(request, requestInit);
+    return fetch(request);
   }
 
   public delete(uri: string): Promise<any> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('User-Agent', 'provide-js client');
+    headers.append('Authorization', `bearer ${this.token}`);
+
     const requestInit: RequestInit = {
       method: 'DELETE',
-      headers: this.headers,
-      mode: "cors",
+      headers: headers,
+      mode: 'cors',
     };
 
-    const request = new Request(this.baseUrl + uri);
+    const request = new Request(this.baseUrl + uri, requestInit);
 
-    return fetch(request, requestInit);
+    return fetch(request);
   }
 
 }
