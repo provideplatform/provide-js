6397d2bb7a8df49a3262d37d051f8f7236ebfcac
Kyle Thomas <kyle@provide.services>
2020-12-15 08:54:31 -0500

    Expose initial privacy package

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,5 @@ export { Bookie, bookieClientFactory } from './bookie';
 export { Capabilities, capabilitiesFactory } from './capabilities';
 export { Ident, identClientFactory } from './ident';
 export { NChain, nchainClientFactory } from './nchain';
+export { Privacy, privacyClientFactory } from './privacy';
 export { Vault, vaultClientFactory } from './vault';
---
+++ src/services/privacy.ts
@@ -0,0 +1,48 @@
+import { ApiClient } from '../clients';
+import { Circuit } from '@provide/types';
+
+/*
+ * Privacy microservice; provides access to protocol-agnostic functionality
+ * exposed by the Provide Privacy API and its underlying circuit registry.
+ */
+export class Privacy {
+
+  private static readonly DEFAULT_HOST = 'privacy.provide.services';
+
+  private readonly client: ApiClient;
+
+  constructor(token: string, scheme?: string, host?: string, path?: string) {
+    if (!host) {
+      host = Privacy.DEFAULT_HOST;
+    }
+
+    this.client = new ApiClient(token, scheme, host, path);
+  }
+
+  public static clientFactory(token: string, scheme?: string, host?: string, path?: string): Privacy {
+    const _scheme = scheme ? scheme : (process.env['PRIVACY_API_SCHEME'] || 'https');
+    const _host = host ? host : (process.env['PRIVACY_API_HOST'] || Privacy.DEFAULT_HOST);
+    const _path = path ? path : (process.env['PRIVACY_API_PATH'] || ApiClient.DEFAULT_PATH);
+    return new Privacy(token, _scheme, _host, _path);
+  }
+
+  public async fetchCircuits(params?: object): Promise<Circuit[]> {
+    return ApiClient.handleResponse(await this.client.get('circuits', params || {}));
+  }
+
+  public async fetchCircuitDetails(circuitId: string, params?: object): Promise<Circuit> {
+    return ApiClient.handleResponse(await this.client.get(`circuits/${circuitId}`, params || {}));
+  }
+
+  public async deployCircuit(params: object): Promise<Circuit> {
+    return ApiClient.handleResponse(await this.client.post('circuits', params));
+  }
+
+  public async verify(circuitId: string, params: object): Promise<boolean> {
+    return ApiClient.handleResponse(await this.client.post(`circuits/${circuitId}/verify`, params));
+  }
+}
+
+export const privacyClientFactory = (token: string, scheme?: string, host?: string, path?: string): Privacy => {
+  return Privacy.clientFactory(token, scheme, host, path);
+};
